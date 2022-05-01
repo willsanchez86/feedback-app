76a95202264f3579f92f6f2e1b2401297411e270
Will <williamlancelotsanchez@gmail.com>
2022-05-01 19:52:24 -0400

    Created and styled FeedbackList, FeedbackItem, shared Card components and managed Global State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import {useState} from 'react';
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+
 function App() {
-    return <h1>Hello from the app component</h1>
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    return (
+        <>
+            <Header text="Feedback UI"/>
+            <div className='container'> 
+                <FeedbackList feedback={feedback} />
+            </div>
+
+        </>
+    )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+/* Must return one parent element - Can be wrapped in empty fragments ('<></>) */
+
+// Can return non-jsx element by using React.createElement
