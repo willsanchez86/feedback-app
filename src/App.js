0ac616bd51f23df99076e8e1ed6df6a9e74c4221
Will <williamlancelotsanchez@gmail.com>
2022-05-02 12:17:41 -0400

    Add Feedback Functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid';
 import {useState} from 'react';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -5,21 +6,25 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 
-function App() {
+function App() { 
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]); //State is immutable so cannot be pushed
+    }
 
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
-        }
-        
+        }    
     }
 
     return (
         <>
             <Header text="Feedback UI"/>
             <div className='container'> 
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback}/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from './shared/Card';
 import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
@@ -11,22 +11,36 @@ function FeedbackForm() {
 
     const handleTextChange = (e) => {
         if(text === '') {
-            setBtnDisabled(true)
-            setMessage(null)
+            setBtnDisabled(true);
+            setMessage(null);
         } else if(text !== '' && text.trim().length <= 10) {
-            setMessage('Text must be at least 10 characters')
-            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters');
+            setBtnDisabled(true);
         } else {
-            setMessage(null)
-            setBtnDisabled(false)
+            setMessage(null);
+            setBtnDisabled(false);
         }
 
         setText(e.target.value);
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating
+            };
+
+            handleAdd(newFeedback);
+
+            setText('');
+        }
+    }
+
     return (
         <Card>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your experience?</h2>
                 <RatingSelect select={(rating) => setRating(rating)}/>
                 <div className="input-group">
