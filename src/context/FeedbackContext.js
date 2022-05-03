1a868dfdee65469883dc6cb2c249f68b98d7b627
Will <williamlancelotsanchez@gmail.com>
2022-05-03 08:54:34 -0400

    UpdateFeedback functioning

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,22 +22,46 @@ export const FeedbackProvider = ({children}) => {
           }
     ]);
 
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }    
-    }
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    });
 
+
+    // Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    };
+
+    // Add Feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
         setFeedback([newFeedback, ...feedback]); //State is immutable so cannot be pushed
     }
 
+    // Delete Feedback
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id));
+        }    
+    }
+
+    // Update Feedback Item
+    const updateFeedback = (id, updItem) => {
+        setFeedback(feedback.map((item) => item.id === id ? updItem : item));
+    }
+
     return <FeedbackContext.Provider 
     value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback,
+        updateFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
